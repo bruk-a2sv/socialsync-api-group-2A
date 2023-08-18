f16c4d7b338ebe0130ab96059e9a1da65a7ee84a
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2023-08-18 10:27:03 +0300

    Namspace Edited

--- Application/Contracts/Persistance/IFollowRepository.cs
+++ Application/Contracts/Persistance/IFollowRepository.cs
@@ -1,5 +1,11 @@
-namespace SocialSync.Application.Contracts.Persistance;
+using Domain.Entities;
+
+namespace Application.Contracts.Persistance;
 
 public interface IFollowRepository{
-    
+
+    Task<List<Follow>> GetFollowing(int id);
+    Task<List<Follow>> GetFollower(int id);
+    Task Follow(Follow follow);
+
 }
